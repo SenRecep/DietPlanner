2509ef8a806f651101d70c23e74d40b3fdd667d9
RECEP ŞEN <me@senrecep.com>
2021-05-26 16:37:23 +0300

    IGenericRepository

---
+++ Layers/DietPlanner.Server.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DietPlanner.Server.Entities.Interfaces;
+
+namespace DietPlanner.Server.DAL.Interfaces
+{
+    public interface IGenericRepository<T> : IUnitOfWork
+        where T : class, IEntityBase, new()
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<IEnumerable<T>> GetAllWithDeletedAsync();
+        public Task<IEnumerable<T>> GetAllByUserIdAsync(Guid id);
+
+        public Task<T> GetByIdAsync(Guid id);
+        public Task<T> GetByUserIdAsync(Guid id);
+
+        public Task<T> AddAsync(T entity);
+        public Task UpdateAsync(T entity);
+
+        public Task RemoveAsync(T entity, bool hardDelete = false);
+
+        public Task<int> SaveChangesAsync();
+    }
+}
---
+++ Layers/DietPlanner.Server.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DietPlanner.Server.DAL.Interfaces
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public Task<bool> Commit(bool state = true);
+    }
+}
