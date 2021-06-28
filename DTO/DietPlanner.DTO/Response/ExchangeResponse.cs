ef302b6ec737689b52bc448a438daff79bb86679
Recep Şen <33979963+SenRecep@users.noreply.github.com>
2021-06-28 21:14:45 +0300

    Templates location changes

---
+++ DTO/DietPlanner.DTO/Response/ExchangeResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace DietPlanner.DTO.Response
+{
+    public class ExchangeResponse
+    {
+        public Motd Motd { get; set; }
+        public bool Success { get; set; }
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public Rates Rates { get; set; }
+    }
+
+    public class Motd
+    {
+        public string Msg { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Rates
+    {
+        public float EUR { get; set; }
+        public float GBP { get; set; }
+        public float USD { get; set; }
+    }
+}
